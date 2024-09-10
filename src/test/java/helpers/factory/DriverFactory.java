e85139d52008b0a8e0e71b4653533a110b8e2963
seyyitaliperse <seyyitaliperse@gmail.com>
2024-09-10 12:31:57 +0300

    - Fixed thread issue

--- src/test/java/helpers/factory/DriverFactory.java
+++ src/test/java/helpers/factory/DriverFactory.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
